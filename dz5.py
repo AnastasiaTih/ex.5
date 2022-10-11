2caea8aa4fa46b33d3a343d332dc362dbfcdd438
AnastasiaTih <114216328+AnastasiaTih@users.noreply.github.com>
2022-10-11 20:56:11 +0300

    Add files via upload

--- dz5.py
+++ dz5.py
@@ -1,20 +1,11 @@
-#1
 A = [1,2,3,5]
 count = 0
-for item in A:
-    if item >= 4:
-        count += 1
-print('количество элементов списка, удовлетворяющих данному условию:', count)
-
-#2
-N = 5
-A = [0] * N
-from random import randint
-for i in range(N):
-    A[i] = randint(0, 50)
-print(A)
-count = 0
-for item in A:
-    if item >= 4:
+min_min = 10
+delta = 4
+for item in range(len(A)):
+    if A[item] < min_min:
+        min_min = A[item]
+for item in range(len(A)):
+    if min_min + delta == A[item]:
         count += 1
 print('количество элементов списка, удовлетворяющих данному условию:', count)
